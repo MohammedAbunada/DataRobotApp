5d569ee0f09755afb8a3a44977776f30c6cf39fe
Mohammed Abunada <m.abunada@salla.sa>
2026-02-01 17:06:38 +0300

    fixed architecture

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py migrate
+python manage.py collectstatic
 
 gunicorn Dashboard.wsgi:application --bind 0.0.0.0:8000 --timeout 1200
